e72f5a31df2cb4eac5063d822451ccc3f9b1e92f
AbhinavVijayvergia <abhinavijay63@gmail.com>
2026-02-23 14:42:15 +0530

    Charan G B Added UsecasE2

--- App/src/UseCase2PalindromeCheckerApp.java
+++ App/src/UseCase2PalindromeCheckerApp.java
@@ -1,2 +1,28 @@
-public class PalindromeCheckerApp1 {
+import java.util.Scanner;
+
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Get input from user
+        System.out.print("Input text: ");
+        String word = scanner.nextLine();
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check palindrome and store boolean result
+        boolean isPalindrome = word.equals(reversed);
+
+        // Print result in required format
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+        scanner.close();
+    }
 }
